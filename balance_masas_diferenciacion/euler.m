c4a8e6bffa08bfffd9f589a59cca13d0cadacf85
Fedx <fedegmz18@gmail.com>
2023-07-03 16:34:45 -0300

    corregido método RK2 (dbo_e) y refactors

--- balance_masas_diferenciacion/euler.m
+++ balance_masas_diferenciacion/euler.m
@@ -10,7 +10,7 @@ function datos_balance = iterar_mes(vn, on, dn, h, dia_inicial, dia_final, numer
   od_e = 2; # g/m^3
   od_s = 9; # g/m^3
   # 20 -> 100%
-  # 17 -> 85%
+  # 16 -> 80%
 
   # Obtengo los caudales y les cambio las unidades a m^3/dia
   qe = 86400 * caudales(numero_mes, 1);
@@ -19,20 +19,21 @@ function datos_balance = iterar_mes(vn, on, dn, h, dia_inicial, dia_final, numer
   datos_balance = zeros(dia_final-dia_inicial+1, 3);
 
   for i = 1:dia_final-dia_inicial+1
-    kbd = kbd_0 * (on.^2 / (on.^2 + ko2));
-    g = ka * (od_s - on);
-    p = kbd * dn;
+    kbd = kbd_0 * (on^2 / (on^2 + ko2));
+    g = ka * vn * (od_s - on);
+    p = kbd * vn * dn;
 
-    fo = ((qe * od_e - qs * on)/vn) + g - p;
-    fd = ((qe * dbo_e - qs * dn)/vn) - p;
+    fv = qe - qs;
+    fo = ((qe*od_e - qs*on) + g - p)/vn;
+    fd = ((qe*dbo_e - qs*dn) - p)/vn;
 
-    datos_balance(i, 1) = vn + h * (qe - qs);
-    datos_balance(i, 2) = on + h * fo;
-    datos_balance(i, 3) = dn + h * fd;
+    vn = vn + h*fv;
+    on = on + h*fo;
+    dn = dn + h*fd;
 
-    vn = datos_balance(i, 1);
-    on = datos_balance(i, 2);
-    dn = datos_balance(i, 3);
+    datos_balance(i, 1) = vn;
+    datos_balance(i, 2) = on;
+    datos_balance(i, 3) = dn;
   endfor
 endfunction
 
@@ -70,83 +71,134 @@ function bal_anual = iterar_12_meses(v0, o0, d0, h)
   endfor
 endfunction
 
-h = 1; #en dias
 v0 = 264.98 * (10.^6); # V0 obtenido por ajuste, y expresado en (m^3)
-o0 = 0; # OD0 = ODe
-d0 = 0; # DBO0 = DBOe
+o0 = 0; # OD0
+d0 = 0; # DBO0
 
-euler_paso_diario = iterar_12_meses(v0, o0, d0, h);
+
+h = 1; #en dias
+euler_paso_diario(1:365, :) = iterar_12_meses(v0, o0, d0, h);
+v = euler_paso_diario(365, 1);
+o = euler_paso_diario(365, 2);
+d = euler_paso_diario(365, 3);
+euler_paso_diario(366:730, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_diario(730, 1);
+o = euler_paso_diario(730, 2);
+d = euler_paso_diario(730, 3);
+euler_paso_diario(731:1095, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_diario(1095, 1);
+o = euler_paso_diario(1095, 2);
+d = euler_paso_diario(1095, 3);
+euler_paso_diario(1096:1460, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_diario(1460, 1);
+o = euler_paso_diario(1460, 2);
+d = euler_paso_diario(1460, 3);
+euler_paso_diario(1461:1825, :) = iterar_12_meses(v, o, d, h);
+
 
 h = 0.5;
+euler_paso_medio_dia(1:730, :) = iterar_12_meses(v0, o0, d0, h);
+v = euler_paso_medio_dia(730, 1);
+o = euler_paso_medio_dia(730, 2);
+d = euler_paso_medio_dia(730, 3);
+euler_paso_medio_dia(731:1460, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_medio_dia(1460, 1);
+o = euler_paso_medio_dia(1460, 2);
+d = euler_paso_medio_dia(1460, 3);
+euler_paso_medio_dia(1461:2190, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_medio_dia(2190, 1);
+o = euler_paso_medio_dia(2190, 2);
+d = euler_paso_medio_dia(2190, 3);
+euler_paso_medio_dia(2191:2920, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_medio_dia(2920, 1);
+o = euler_paso_medio_dia(2920, 2);
+d = euler_paso_medio_dia(2920, 3);
+euler_paso_medio_dia(2921:3650, :) = iterar_12_meses(v, o, d, h);
 
-euler_paso_medio_dia = iterar_12_meses(v0, o0, d0, h);
 
 h = 7;
+euler_paso_semanal(1:52, :) = iterar_12_meses(v0, o0, d0, h);
+v = euler_paso_semanal(52, 1);
+o = euler_paso_semanal(52, 2);
+d = euler_paso_semanal(52, 3);
+euler_paso_semanal(53:104, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_semanal(104, 1);
+o = euler_paso_semanal(104, 2);
+d = euler_paso_semanal(104, 3);
+euler_paso_semanal(105:156, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_semanal(156, 1);
+o = euler_paso_semanal(156, 2);
+d = euler_paso_semanal(156, 3);
+euler_paso_semanal(157:208, :) = iterar_12_meses(v, o, d, h);
+v = euler_paso_semanal(208, 1);
+o = euler_paso_semanal(208, 2);
+d = euler_paso_semanal(208, 3);
+euler_paso_semanal(209:260, :) = iterar_12_meses(v, o, d, h);
 
-euler_paso_semanal = iterar_12_meses(v0, o0, d0, h);
+et_euler_semanal = abs(euler_paso_semanal(53:260, 2:3) - euler_paso_medio_dia(744:14:3650, 2:3));
+et_euler_diario = abs(euler_paso_diario(366:1825, 2:3) - euler_paso_medio_dia(732:2:3650, 2:3));
 
-et_euler_semanal = abs(euler_paso_semanal(:, 2:3) - euler_paso_medio_dia(14:14:730, 2:3));
-et_euler_diario = abs(euler_paso_diario(:, 2:3) - euler_paso_medio_dia(2:2:730, 2:3));
+# desplazo los datos calculados para graficarlos desde el 0
+euler_paso_diario(2:1826, :) = euler_paso_diario(1:1825, :);
+euler_paso_diario(1, :) = [v0, o0, d0];
 
-euler_paso_diario(2:366, :) = euler_paso_diario(1:365, :); # desplazo los datos calculados
-euler_paso_diario(1, :) = [v0, o0, d0]; # coloco las condiciones iniciales en la matriz
+euler_paso_semanal(2:261, :) = euler_paso_semanal(1:260, :);
+euler_paso_semanal(1, :) = [v0, o0, d0];
 
 # Grafico de Volumen
-plot(0:365, euler_paso_diario(:, 1));
+plot(0:1825, euler_paso_diario(:, 1));
 legend("Volumen");
-title("Volumen simulado en 1 año");
+title("Volumen simulado en 5 años");
 xlabel ("t (dias)");
 ylabel ("V(t)");
 hold off;
 
-print -djpeg volumen_simulado_euler.jpg;
+print -djpeg euler_volumen_simulado.jpg;
 
 # Graficos de concentracion con paso Diario
-plot(0:365, euler_paso_diario(:, 2));
+plot(0:1825, euler_paso_diario(:, 2));
 hold on;
-plot(0:365, euler_paso_diario(:, 3));
+plot(0:1825, euler_paso_diario(:, 3));
 legend("OD", "DBO");
-title("Concentración de OD y DBO simulado en un año");
+title("Concentración de OD y DBO simuladas en 5 años");
 xlabel ("t (dias)");
 ylabel ("C(t)");
 hold off;
 
-print -djpeg euler_anual_paso_diario.jpg;
+print -djpeg euler_paso_diario.jpg;
 
 # Grafico errores de truncamiento con paso diario
-plot(0:365, [0; et_euler_diario(:, 1)]); # grafico et OD
+plot(0:1460, [0; et_euler_diario(:, 1)]); # grafico et OD
 hold on;
-plot(0:365, [0; et_euler_diario(:, 2)]); # grafico et DBO
+plot(0:1460, [0; et_euler_diario(:, 2)]); # grafico et DBO
 legend("Et OD", "Et DBO");
 title("Errores de truncamiento de OD y DBO con paso diario");
 xlabel ("t (dias)");
 ylabel ("Et(t)");
 hold off;
 
-print -djpeg errores_truncamiento_diario_euler.jpg;
+print -djpeg euler_errores_truncamiento_diario.jpg;
 
 # Grafico errores de truncamiento con paso semanal
-plot(0:52, [0; et_euler_semanal(:, 1)]); # grafico et OD
+plot(0:208, [0; et_euler_semanal(:, 1)]); # grafico et OD
 hold on;
-plot(0:52, [0; et_euler_semanal(:, 2)]); # grafico et DBO
+plot(0:208, [0; et_euler_semanal(:, 2)]); # grafico et DBO
 legend("Et OD", "Et DBO");
 title("Error de truncamiento de OD y DBO con paso semanal");
 xlabel ("t (semanas)");
 ylabel ("Et(t)");
 hold off;
 
-print -djpeg errores_truncamiento_semanal_euler.jpg;
+print -djpeg euler_errores_truncamiento_semanal.jpg;
 
 # Grafico con paso semanal
-plot(1:52, euler_paso_semanal(:, 2)); # grafico et OD
+plot(0:260, euler_paso_semanal(:, 2)); # grafico et OD
 hold on;
-plot(1:52, euler_paso_semanal(:, 3)); # grafico et DBO
+plot(0:260, euler_paso_semanal(:, 3)); # grafico et DBO
 legend("OD", "DBO");
-title("Concentración de OD y DBO estimada con paso semanal");
+title("Concentración de OD y DBO simuladas en 5 años");
 xlabel ("t (semanas)");
 ylabel ("C(t)");
 hold off;
 
-print -djpeg euler_anual_semanal.jpg;
-
-
+print -djpeg euler_semanal.jpg;
--- balance_masas_diferenciacion/runge_kutta.m
+++ balance_masas_diferenciacion/runge_kutta.m
@@ -10,19 +10,19 @@ function datos_balance = iterar_mes(vn, on, dn, h, dia_inicial, dia_final, numer
   od_e = 2; # g/m^3
   od_s = 9; # g/m^3
   # 20 -> 100%
-  # 10 -> 50%
-
-  # Obtengo los caudales y les cambio las unidades a m^3/dia
+  # 16 -> 80%
 
   datos_balance = zeros(dia_final-dia_inicial+1, 3);
 
+  # Obtengo los caudales y les cambio las unidades a m^3/dia
   qe1 = 86400 * caudales(numero_mes, 1);
   qs1 = 86400 * caudales(numero_mes, 2);
 
   qe2 = qe1;
   qs2 = qs1;
 
-  for i = 1:dia_final-dia_inicial+1
+   for i = 1:dia_final-dia_inicial+1
+
     if(i == dia_final-dia_inicial+1 && numero_mes != 12)
       qe2 = 86400 * caudales(numero_mes+1, 1); # qe(tn+1)
       qs2 = 86400 * caudales(numero_mes+1, 2); # qs(tn+1)
@@ -37,26 +37,26 @@ function datos_balance = iterar_mes(vn, on, dn, h, dia_inicial, dia_final, numer
     q2v = h*(qe2 - qs2);
 
     kbd1 = kbd_0 * (on.^2 / (on.^2 + ko2));
-    g1 = ka * (od_s - on);
-    p1 = kbd1 * dn;
+    g1 = ka * vn * (od_s - on);
+    p1 = kbd1 * vn * dn;
 
-    q1o = h*(((qe1 * od_e - qs1 * on)/vn) + g1 - p1);
-    q1d = h*(((qe1 * dbo_e - qs1 * dn)/vn) - p1);
+    q1o = h*(((qe1 * od_e - qs1 * on) + g1 - p1)/vn);
+    q1d = h*(((qe1 * dbo_e - qs1 * dn) - p1)/vn);
 
     kbd2 = kbd_0 * ((on + q1o).^2 / ((on + q1o).^2 + ko2));
-    g2 = ka * (od_s - (on + q1o));
-    p2 = kbd2 * (dn + q1d);
+    g2 = ka * (vn+q1v) * (od_s - on - q1o);
+    p2 = kbd2 * (vn+q1v) * (dn + q1d);
 
-    q2o = h*(((qe2 * od_e - qs2 * (on + q1o))/(vn + q1v)) + g2 - p2);
-    q2d = h*(((qe2 * od_e - qs2 * (on + q1o))/(vn + q1v)) - p2);
+    q2o = h*(((qe2 * od_e - qs2 * (on + q1o)) + g2 - p2)/(vn+q1v));
+    q2d = h*(((qe2 * dbo_e - qs2 * (dn + q1d)) - p2)/(vn + q1v));
 
-    datos_balance(i, 1) = vn + ((1/2) .* (q1v + q2v));
-    datos_balance(i, 2) = on + ((1/2) .* (q1o + q2o));
-    datos_balance(i, 3) = dn + ((1/2) .* (q1d + q2d));
+    vn = vn + ((0.5) * (q1v + q2v));
+    on = on + ((0.5) * (q1o + q2o));
+    dn = dn + ((0.5) * (q1d + q2d));
 
-    vn = datos_balance(i, 1);
-    on = datos_balance(i, 2);
-    dn = datos_balance(i, 3);
+    datos_balance(i, 1) = vn;
+    datos_balance(i, 2) = on;
+    datos_balance(i, 3) = dn;
   endfor
 endfunction
 
@@ -67,76 +67,176 @@ function bal_anual = iterar_12_meses(v0, o0, d0, h)
   for i = 1:12
     switch(i)
     case 1
-      bal_anual(1:round(31.*1/h), :) = iterar_mes(v0, o0, d0, h, 1, round(31 .* 1/h), i);
+      bal_anual(1:round(31/h), :) = iterar_mes(v0, o0, d0, h, 1, round(31/h), i);
     case 2
-      bal_anual(round(31*1/h)+1:round(59.*1/h), :) = iterar_mes(bal_anual(round(31.*1/h), 1), bal_anual(round(31.*1/h), 2), bal_anual(round(31.*1/h), 3), h, round(31.*1/h)+1, round(59.*1/h), i);
+      bal_anual(round(31/h)+1:round(59/h), :) = iterar_mes(bal_anual(round(31/h), 1), bal_anual(round(31/h), 2), bal_anual(round(31/h), 3), h, round(31/h)+1, round(59/h), i);
     case 3
-      bal_anual(round(59.*1/h)+1:round(90.*1/h), :) = iterar_mes(bal_anual(round(59.*1/h), 1), bal_anual(round(59.*1/h), 2), bal_anual(round(59.*1/h), 3), h, round(59.*1/h)+1, round(90.*1/h), i);
+      bal_anual(round(59/h)+1:round(90/h), :) = iterar_mes(bal_anual(round(59/h), 1), bal_anual(round(59/h), 2), bal_anual(round(59/h), 3), h, round(59/h)+1, round(90/h), i);
     case 4
-      bal_anual(round(90*1/h)+1:round(112.*1/h), :) = iterar_mes(bal_anual(round(90.*1/h), 1), bal_anual(round(90.*1/h), 2), bal_anual(round(90.*1/h), 3), h, round(90*1/h)+1, round(112.*1/h), i);
+      bal_anual(round(90/h)+1:round(112/h), :) = iterar_mes(bal_anual(round(90.*1/h), 1), bal_anual(round(90.*1/h), 2), bal_anual(round(90.*1/h), 3), h, round(90*1/h)+1, round(112.*1/h), i);
     case 5
-      bal_anual(round(112*1/h)+1:round(144.*1/h), :) = iterar_mes(bal_anual(round(112.*1/h), 1), bal_anual(round(112.*1/h), 2), bal_anual(round(112.*1/h), 3), h, round(112*1/h)+1, round(144.*1/h), i);
+      bal_anual(round(112/h)+1:round(144/h), :) = iterar_mes(bal_anual(round(112.*1/h), 1), bal_anual(round(112.*1/h), 2), bal_anual(round(112.*1/h), 3), h, round(112*1/h)+1, round(144.*1/h), i);
     case 6
-      bal_anual(round(144*1/h)+1:round(175.*1/h), :) = iterar_mes(bal_anual(round(144.*1/h), 1), bal_anual(round(144.*1/h), 2), bal_anual(round(144.*1/h), 3), h, round(144*1/h)+1, round(175.*1/h), i);
+      bal_anual(round(144/h)+1:round(175/h), :) = iterar_mes(bal_anual(round(144.*1/h), 1), bal_anual(round(144.*1/h), 2), bal_anual(round(144.*1/h), 3), h, round(144*1/h)+1, round(175.*1/h), i);
     case 7
-      bal_anual(round(175*1/h)+1:round(207.*1/h), :) = iterar_mes(bal_anual(round(175.*1/h), 1), bal_anual(round(175.*1/h), 2), bal_anual(round(175.*1/h), 3), h, round(175*1/h)+1, round(207.*1/h), i);
+      bal_anual(round(175/h)+1:round(207/h), :) = iterar_mes(bal_anual(round(175.*1/h), 1), bal_anual(round(175.*1/h), 2), bal_anual(round(175.*1/h), 3), h, round(175*1/h)+1, round(207.*1/h), i);
     case 8
-      bal_anual(round(207*1/h)+1:round(239.*1/h), :) = iterar_mes(bal_anual(round(207.*1/h), 1), bal_anual(round(207.*1/h), 2), bal_anual(round(207.*1/h), 3), h, round(207*1/h)+1, round(239.*1/h), i);
+      bal_anual(round(207/h)+1:round(239/h), :) = iterar_mes(bal_anual(round(207.*1/h), 1), bal_anual(round(207.*1/h), 2), bal_anual(round(207.*1/h), 3), h, round(207*1/h)+1, round(239.*1/h), i);
     case 9
-      bal_anual(round(239*1/h)+1:round(270.*1/h), :) = iterar_mes(bal_anual(round(239.*1/h), 1), bal_anual(round(239.*1/h), 2), bal_anual(round(239.*1/h), 3), h, round(239*1/h)+1, round(270.*1/h), i);
+      bal_anual(round(239*1/h)+1:round(270/h), :) = iterar_mes(bal_anual(round(239.*1/h), 1), bal_anual(round(239.*1/h), 2), bal_anual(round(239.*1/h), 3), h, round(239*1/h)+1, round(270.*1/h), i);
     case 10
-      bal_anual(round(270*1/h)+1:round(302.*1/h), :) = iterar_mes(bal_anual(round(270.*1/h), 1), bal_anual(round(270.*1/h), 2), bal_anual(round(270.*1/h), 3), h, round(270*1/h)+1, round(302.*1/h), i);
+      bal_anual(round(270/h)+1:round(302/h), :) = iterar_mes(bal_anual(round(270.*1/h), 1), bal_anual(round(270.*1/h), 2), bal_anual(round(270.*1/h), 3), h, round(270*1/h)+1, round(302.*1/h), i);
     case 11
-      bal_anual(round(302*1/h)+1:round(333.*1/h), :) = iterar_mes(bal_anual(round(302.*1/h), 1), bal_anual(round(302.*1/h), 2), bal_anual(round(302.*1/h), 3), h, round(302*1/h)+1, round(333.*1/h), i);
+      bal_anual(round(302/h)+1:round(333/h), :) = iterar_mes(bal_anual(round(302.*1/h), 1), bal_anual(round(302.*1/h), 2), bal_anual(round(302.*1/h), 3), h, round(302*1/h)+1, round(333.*1/h), i);
     case 12
-      bal_anual(round(333*1/h)+1:round(365.*(1/h)), :) = iterar_mes(bal_anual(round(333.*1/h), 1), bal_anual(round(333.*1/h), 2), bal_anual(round(333.*1/h), 3), h, round(333*1/h)+1, round(365.*(1/h)), i);
+      bal_anual(round(333/h)+1:round(365/h), :) = iterar_mes(bal_anual(round(333.*1/h), 1), bal_anual(round(333.*1/h), 2), bal_anual(round(333.*1/h), 3), h, round(333*1/h)+1, round(365.*(1/h)), i);
     endswitch
   endfor
 endfunction
 
-h = 1; #en dias
+
 v0 = 264.98 * (10.^6); # V0 obtenido por ajuste, y expresado en (m^3)
-o0 = 0; # OD0 = OD0
-d0 = 0; # DBO0 = DBO0
+o0 = 0; # OD0
+d0 = 0; # DBO0
 
-rungekutta_paso_diario = iterar_12_meses(v0, o0, d0, h);
 
-h = 0.5;
+h = 1; #en dias
+rungekutta_paso_diario(1:365, :) = iterar_12_meses(v0, o0, d0, h);
+v = rungekutta_paso_diario(365, 1);
+o = rungekutta_paso_diario(365, 2);
+d = rungekutta_paso_diario(365, 3);
+rungekutta_paso_diario(366:730, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_diario(730, 1);
+o = rungekutta_paso_diario(730, 2);
+d = rungekutta_paso_diario(730, 3);
+rungekutta_paso_diario(731:1095, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_diario(1095, 1);
+o = rungekutta_paso_diario(1095, 2);
+d = rungekutta_paso_diario(1095, 3);
+rungekutta_paso_diario(1096:1460, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_diario(1460, 1);
+o = rungekutta_paso_diario(1460, 2);
+d = rungekutta_paso_diario(1460, 3);
+rungekutta_paso_diario(1461:1825, :) = iterar_12_meses(v, o, d, h);
 
-rungekutta_paso_medio_dia = iterar_12_meses(v0, o0, d0, h);
 
-h = 7;
+h = 0.5; #en dias
+rungekutta_paso_medio_dia(1:730, :) = iterar_12_meses(v0, o0, d0, h);
+v = rungekutta_paso_medio_dia(730, 1);
+o = rungekutta_paso_medio_dia(730, 2);
+d = rungekutta_paso_medio_dia(730, 3);
+rungekutta_paso_medio_dia(731:1460, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_medio_dia(1460, 1);
+o = rungekutta_paso_medio_dia(1460, 2);
+d = rungekutta_paso_medio_dia(1460, 3);
+rungekutta_paso_medio_dia(1461:2190, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_medio_dia(2190, 1);
+o = rungekutta_paso_medio_dia(2190, 2);
+d = rungekutta_paso_medio_dia(2190, 3);
+rungekutta_paso_medio_dia(2191:2920, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_medio_dia(2920, 1);
+o = rungekutta_paso_medio_dia(2920, 2);
+d = rungekutta_paso_medio_dia(2920, 3);
+rungekutta_paso_medio_dia(2921:3650, :) = iterar_12_meses(v, o, d, h);
 
+
+h = 7; #en dias
 rungekutta_paso_semanal = iterar_12_meses(v0, o0, d0, h);
-et_rungekutta_semanal = abs(rungekutta_paso_semanal(:, 2:3) - rungekutta_paso_medio_dia(14:14:730, 2:3));
-et_rungekutta_diario = abs(rungekutta_paso_diario(:, 2:3) - rungekutta_paso_medio_dia(2:2:730, 2:3));
+rungekutta_paso_semanal(1:52, :) = iterar_12_meses(v0, o0, d0, h);
+v = rungekutta_paso_semanal(52, 1);
+o = rungekutta_paso_semanal(52, 2);
+d = rungekutta_paso_semanal(52, 3);
+rungekutta_paso_semanal(53:104, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_semanal(104, 1);
+o = rungekutta_paso_semanal(104, 2);
+d = rungekutta_paso_semanal(104, 3);
+rungekutta_paso_semanal(105:156, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_semanal(156, 1);
+o = rungekutta_paso_semanal(156, 2);
+d = rungekutta_paso_semanal(156, 3);
+rungekutta_paso_semanal(157:208, :) = iterar_12_meses(v, o, d, h);
+v = rungekutta_paso_semanal(208, 1);
+o = rungekutta_paso_semanal(208, 2);
+d = rungekutta_paso_semanal(208, 3);
+rungekutta_paso_semanal(209:260, :) = iterar_12_meses(v, o, d, h);
 
-rungekutta_paso_diario(2:366, :) = rungekutta_paso_diario(1:365, :); # desplazo los datos calculados
-rungekutta_paso_diario(1, :) = [v0, o0, d0]; # coloco las condiciones iniciales en la matriz
 
+et_rungekutta_semanal = abs(rungekutta_paso_semanal(53:260, 2:3) - rungekutta_paso_medio_dia(744:14:3650, 2:3));
+et_rungekutta_diario = abs(rungekutta_paso_diario(366:1825, 2:3) - rungekutta_paso_medio_dia(732:2:3650, 2:3));
+
+# desplazo los datos calculados para graficarlos desde el 0
+rungekutta_paso_diario(2:1826, :) = rungekutta_paso_diario(1:1825, :);
+rungekutta_paso_diario(1, :) = [v0, o0, d0];
+
+rungekutta_paso_semanal(2:261, :) = rungekutta_paso_semanal(1:260, :);
+rungekutta_paso_semanal(1, :) = [v0, o0, d0];
+
+# Grafico de Volumen
+plot(0:1825, rungekutta_paso_diario(:, 1));
+legend("Volumen");
+title("Volumen simulado en 5 años");
+xlabel ("t (dias)");
+ylabel ("V(t)");
+hold off;
+
+print -djpeg rungekutta_volumen_simulado.jpg;
 
 # Graficos de concentracion con paso Diario
-plot(0:365, rungekutta_paso_diario(:, 2));
+plot(0:1825, rungekutta_paso_diario(:, 2));
 hold on;
-plot(0:365, rungekutta_paso_diario(:, 3));
+plot(0:1825, rungekutta_paso_diario(:, 3));
 legend("OD", "DBO");
-title("Concentración de OD y DBO simulado en un año");
+title("Concentración de OD y DBO simulado en 5 años");
 xlabel ("t (dias)");
 ylabel ("C(t)");
 hold off;
 
-print -djpeg rungekutta_od_minimo_4.jpg;
+print -djpeg rungekutta_paso_diario.jpg;
+
+# Graficos de concentracion con paso Diario
+plot(0:1825, rungekutta_paso_diario(:, 2));
+hold on;
+plot(0:1825, rungekutta_paso_diario(:, 3));
+legend("OD", "DBO");
+title("Concentración de OD y DBO simuladas en 5 años");
+xlabel ("t (dias)");
+ylabel ("C(t)");
+hold off;
+
+print -djpeg rungekutta_paso_diario.jpg;
+
+# Grafico errores de truncamiento con paso diario
+plot(0:1460, [0; et_rungekutta_diario(:, 1)]); # grafico et OD
+hold on;
+plot(0:1460, [0; et_rungekutta_diario(:, 2)]); # grafico et DBO
+legend("Et OD", "Et DBO");
+title("Errores de truncamiento de OD y DBO con paso diario");
+xlabel ("t (dias)");
+ylabel ("Et(t)");
+hold off;
+
+print -djpeg rungekutta_errores_truncamiento_diario.jpg;
+
+# Grafico errores de truncamiento con paso semanal
+plot(0:208, [0; et_rungekutta_semanal(:, 1)]); # grafico et OD
+hold on;
+plot(0:208, [0; et_rungekutta_semanal(:, 2)]); # grafico et DBO
+legend("Et OD", "Et DBO");
+title("Error de truncamiento de OD y DBO con paso semanal");
+xlabel ("t (semanas)");
+ylabel ("Et(t)");
+hold off;
+
+print -djpeg rungekutta_errores_truncamiento_semanal.jpg;
 
 # Grafico con paso semanal
-plot(1:52, rungekutta_paso_semanal(:, 2)); # grafico et OD
+plot(0:260, rungekutta_paso_semanal(:, 2)); # grafico et OD
 hold on;
-plot(1:52, rungekutta_paso_semanal(:, 3)); # grafico et DBO
+plot(0:260, rungekutta_paso_semanal(:, 3)); # grafico et DBO
 legend("OD", "DBO");
-title("Concentración de OD y DBO estimada con paso semanal");
+title("Concentración de OD y DBO simuladas en 5 años");
 xlabel ("t (semanas)");
 ylabel ("C(t)");
 hold off;
 
-print -djpeg rungekutta_anual_semanal.jpg;
-
-
+print -djpeg rungekutta_paso_semanal.jpg;
